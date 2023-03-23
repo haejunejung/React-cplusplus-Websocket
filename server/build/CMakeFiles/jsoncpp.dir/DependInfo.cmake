99ba8d704fba92e372f4c10720f7f8fe1f4fb012
haejunejung <gowns0408@gmail.com>
2023-03-23 11:22:59 +0900

    server

---
+++ server/build/CMakeFiles/jsoncpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/external/jsoncpp.cpp" "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/build/CMakeFiles/jsoncpp.dir/external/jsoncpp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_WEBSOCKETPP_CPP11_STRICT_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/WebsocketServer.cpp" "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/build/CMakeFiles/server.dir/WebsocketServer.cpp.o"
+  "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/server.cpp" "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/build/CMakeFiles/server.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_WEBSOCKETPP_CPP11_STRICT_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/junghaejune/haejune/tracking-system/websocket-server-demo/server/build/CMakeFiles/jsoncpp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
